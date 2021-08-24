25d806a2556548daebc48455bf804f1193c2a5e3
Solo <anqi.huang@outlook.com>
2021-08-24 19:41:02 +0800

    add jni demo
    Signed-off-by: Solo <anqi.huang@outlook.com>

--- library-native/src/com/journeyOS/jni/JniHelper.java
+++ library-native/src/com/journeyOS/jni/JniHelper.java
@@ -1,10 +1,16 @@
 package com.journeyOS.jni;
 
+import java.util.List;
+
 public class JniHelper {
     static {
         System.loadLibrary("native_weather");
     }
 
     public static native String[] getWeatherKeys();
+
+    public static native List<WeatherKey> getNativeWeatherKeys();
+
+//    public static native WeatherKey getNativeWeatherKey();
 
 }
--- library-native/src/jni/JniHelper.h
+++ library-native/src/jni/JniHelper.h
@@ -22,12 +22,35 @@ static const char *native_library_class_path_name = "com/journeyOS/jni/JniHelper
 
 jobjectArray jni_native_get_weather_key(JNIEnv *env, jclass thiz);
 
+jobject jni_native_get_weather_keys(JNIEnv *env, jclass thiz);
 
+//jobject jni_native_get_weather_key(JNIEnv *env, jclass thiz);
+
+/**
+ * 有部分同学说看了文档还是不知道signature改怎么写
+ * 很简单javap -s JniHelper.class即可，信息如下：
+ * Compiled from "JniHelper.java"
+ *  public class com.journeyOS.jni.JniHelper {
+ *      public com.journeyOS.jni.JniHelper();
+ *          descriptor: ()V
+ *
+ *      public static native java.lang.String[] getWeatherKeys();
+ *          descriptor: ()[Ljava/lang/String;
+ *
+ *      public static native java.util.List<com.journeyOS.jni.WeatherKey> getNativeWeatherKeys();
+ *          descriptor: ()Ljava/util/List;
+ *
+ *      static {};
+ *          descriptor: ()V
+ *
+ */
 static JNINativeMethod native_library_methods[] = {
         //name：Java中函数的名字
         //signature：描述了函数的参数和返回值（Java类型跟C类型对应表可自行网上查询）
         //fnPtr：C/C++的函数名
-        {"getWeatherKeys", "()[Ljava/lang/String;", (void *) jni_native_get_weather_key},
+        {"getWeatherKeys",       "()[Ljava/lang/String;", (void *) jni_native_get_weather_key},
+        {"getNativeWeatherKeys", "()Ljava/util/List;",    (void *) jni_native_get_weather_keys},
+//        {"getNativeWeatherKey", "()Lcom/journeyOS/jni/WeatherKey;",    (void *) jni_native_get_weather_key},
 };
 
 #endif //_JNI_HELPER_H
